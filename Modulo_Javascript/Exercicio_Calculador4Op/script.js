e6bc6076cd776ddf3da3c022b00707b03c894b3c
gustaRC <123694354+gustaRC@users.noreply.github.com>
2023-04-18 09:29:47 -0300

    Implementado Sistema de Menu Principal

--- Modulo_Javascript/Exercicio_Calculador4Op/script.js
+++ Modulo_Javascript/Exercicio_Calculador4Op/script.js
@@ -1,8 +1,8 @@
 let primeiroN = parseFloat(window.prompt("Digite o primeiro número: "))
 let segundoN = parseFloat(window.prompt("Digite o segundo número: "))
 
-console.log("Soma: " + (primeiroN + segundoN))
-console.log("Subtração: " + (primeiroN - segundoN))
-console.log("Multiplicação: " + (primeiroN * segundoN))
-console.log("Divisão: " + (primeiroN / segundoN))
-
+alert("Resultado das Operações:\n" +
+"Soma: " + (primeiroN + segundoN) + 
+"\nSubtração: " + (primeiroN - segundoN) +
+"\nMultiplicação: " + (primeiroN * segundoN) + 
+"\nDivisão: " + (primeiroN / segundoN))
