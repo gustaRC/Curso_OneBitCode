caca6dedc79c5e246b14a2276742b8ad619b6ae2
gustaRC <123694354+gustaRC@users.noreply.github.com>
2023-05-23 17:38:38 -0300

    update 23.05 final

--- Modulo_DOM/Exercicio_JogoDaVelha/script.js
+++ Modulo_DOM/Exercicio_JogoDaVelha/script.js
@@ -1,68 +1,94 @@
 //Exercício - Jogo da Velha
 
-let playerX = []
-let playerO = []
+let playerX = [];
+let playerO = [];
 
-let round = true
+let round = true;
 
-let inputCurrentPlayer = document.getElementById("nameCurrentPlayer")
+let inputCurrentPlayer = document.getElementById("nameCurrentPlayer");
 
 //BOTAO COMEÇAR JOGO!
 document.getElementById("startBtn").addEventListener("click", function () {
-    const namePlayerX = prompt("Nome do Jogador X:")
-    const namePlayerO = prompt("Nome do Jogador O:")
-    inputCurrentPlayer.value = namePlayerX
+  const namePlayerX = prompt("Nome do Jogador X:");
+  const namePlayerO = prompt("Nome do Jogador O:");
+  inputCurrentPlayer.value = namePlayerX;
 
-    let btnGame = document.querySelectorAll(".btnCaseGame").forEach(function (ev) {
-        ev.addEventListener("click", function (btnCase) {
-            const btn = btnCase.currentTarget
-            if (round) {
-                playerX.push(btn.id)
-                btn.innerText = "X"
-                round = false
-                inputCurrentPlayer.value = namePlayerO
-            } else {
-                playerO.push(btn.id)
-                btn.innerText = "O"
-                round = true
-                inputCurrentPlayer.value = namePlayerX
-            }
-
-            btn.setAttribute("disabled", true)
-    
-            winnerFunction()
-        })
-    })
-
-    function winnerFunction() {
-        let currentPlayer = round ? playerX : playerO
-        if (currentPlayer.includes("btn1" && "btn2" && "btn3")) {
-            infoWin()
-        } else if (currentPlayer.includes("btn4" && "btn5" && "btn6")) {
-            infoWin()
-        } else if (currentPlayer.includes("btn7" && "btn8" && "btn9")) {
-            infoWin()
-        } else if (currentPlayer.includes("btn1" && "btn4" && "btn7")) {
-            infoWin()
-        } else if (currentPlayer.includes("btn2" && "btn5" && "btn8")) {
-            infoWin()
-        } else if (currentPlayer.includes("btn3" && "btn6" && "btn9")) {
-            infoWin()
-        } else if (currentPlayer.includes("btn1" && "btn5" && "btn9")) {
-            infoWin()
-        } else if (currentPlayer.includes("btn3" && "btn5" && "btn7")) {
-            infoWin()
+  let btnGame = document
+    .querySelectorAll(".btnCaseGame")
+    .forEach(function (ev) {
+      ev.addEventListener("click", function (btnCase) {
+        const btn = btnCase.currentTarget;
+        if (round) {
+          playerX.push(btn.id);
+          btn.innerText = "X";
+          round = false;
+          inputCurrentPlayer.value = namePlayerO;
+          console.log(playerX);
+        } else {
+          playerO.push(btn.id);
+          btn.innerText = "O";
+          round = true;
+          inputCurrentPlayer.value = namePlayerX;
         }
-    }
+
+        btn.setAttribute("disabled", true);
+
+        winnerFunction();
+      });
+    });
+
+  function winnerFunction() {
+    let currentTarget = [playerX, playerO];
+    currentTarget.forEach(function (ev) {
+      if (ev.includes("btn1") && ev.includes("btn2") && ev.includes("btn3")) {
+        infoWin();
+      } else if (
+        ev.includes("btn4") &&
+        ev.includes("btn5") &&
+        ev.includes("btn6")
+      ) {
+        infoWin();
+      } else if (
+        ev.includes("btn7") &&
+        ev.includes("btn8") &&
+        ev.includes("btn9")
+      ) {
+        infoWin();
+      } else if (
+        ev.includes("btn1") &&
+        ev.includes("btn4") &&
+        ev.includes("btn7")
+      ) {
+        infoWin();
+      } else if (
+        ev.includes("btn2") &&
+        ev.includes("btn5") &&
+        ev.includes("btn8")
+      ) {
+        infoWin();
+      } else if (
+        ev.includes("btn3") &&
+        ev.includes("btn6") &&
+        ev.includes("btn9")
+      ) {
+        infoWin();
+      } else if (
+        ev.includes("btn1") &&
+        ev.includes("btn5") &&
+        ev.includes("btn9")
+      ) {
+        infoWin();
+      } else if (
+        ev.includes("btn3") &&
+        ev.includes("btn5") &&
+        ev.includes("btn7")
+      ) {
+        infoWin();
+      }
+    });
 
     function infoWin() {
-        console.log("Teste")
+      console.log("Teste");
     }
-})
-
-
-
-
-
-
-
+  }
+});
