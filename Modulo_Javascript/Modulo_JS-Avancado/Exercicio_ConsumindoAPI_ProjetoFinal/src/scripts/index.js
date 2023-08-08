24641685f52e0baae2f9049587bbc7c5ecf54d11
gustaRC <123694354+gustaRC@users.noreply.github.com>
2023-08-08 08:15:07 -0300

    update 07.08

--- Modulo_Javascript/Modulo_JS-Avancado/Exercicio_ConsumindoAPI_ProjetoFinal/src/scripts/index.js
+++ Modulo_Javascript/Modulo_JS-Avancado/Exercicio_ConsumindoAPI_ProjetoFinal/src/scripts/index.js
@@ -1,10 +1,28 @@
-import { criarSpan } from "./tagsHtml.js";
+import { criarSpan } from "./criarSpan.js";
 
-async function loadTransacao() {
+document.addEventListener("DOMContentLoaded", async () => {
     const spanTransacoes = await fetch("http://localhost:3000/transacoes").then(result => result.json())
     spanTransacoes.forEach(criarSpan)
-}
+})
 
-document.addEventListener("DOMContentLoaded", () => {
-    loadTransacao()
+const formOP = document.getElementById("formOp")
+formOP.addEventListener("submit", async (ev) => {
+    ev.preventDefault();
+
+    const op = {
+        nome: document.querySelector("#nomeForm").value,
+        valor: document.querySelector("#valorForm").value
+    }
+
+    const response = await fetch("http://localhost:3000/transacoes", {
+        method: "POST",
+        headers: {
+            'Content-Type': "application/json"
+        },
+        body: JSON.stringify(op)
+    })
+
+    const dadosSalvos = await response.json()
+    formOP.reset()
+    criarSpan(dadosSalvos)
 })
